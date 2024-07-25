6d5eb891d6851a1fda1a2d05d6b07683266d5332
DovgaNik <nikita.dovgan.05@hotmail.com>
2024-07-25 19:28:08 +0300

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{thread, time::Duration};
 
 const MAP_SIZE: usize = 15;
 const T_STEP: u64 = 500;
-const P_TRUE: f64 = 0.1;
+const P_TRUE: f64 = 0.5;
 
 fn main() {
     let mut rng = rand::thread_rng();
@@ -11,23 +11,31 @@ fn main() {
     loop {
         let mut a: [[bool; MAP_SIZE]; MAP_SIZE] = [[false; MAP_SIZE]; MAP_SIZE];
 
-        for sub_arr in 0..MAP_SIZE {
-            for element in 0..MAP_SIZE {
-                a[sub_arr][element] = rng.gen_bool(P_TRUE);
-            }
-        }
+        init_map(&mut a, &mut rng);
 
-        for sub_arr in a {
-            for element in sub_arr {
-                if element == true {
-                    print!("██");
-                } else {
-                    print!("  ");
-                }
-            }
-            print!("\n");
-        }
-        print!("\n");
+        print_screen(a);
         thread::sleep(Duration::from_millis(T_STEP));
     }
+}
+
+fn init_map(a: &mut [[bool; 15]; 15], rng: &mut rand::prelude::ThreadRng) {
+    for sub_arr in 0..MAP_SIZE {
+        for element in 0..MAP_SIZE {
+            a[sub_arr][element] = rng.gen_bool(P_TRUE);
+        }
+    }
+}
+
+fn print_screen(a: [[bool; MAP_SIZE]; MAP_SIZE]) {
+    for sub_arr in a {
+        for element in sub_arr {
+            if element == true {
+                print!("██");
+            } else {
+                print!("  ");
+            }
+        }
+        print!("\n");
+    }
+    print!("\n");
 }
