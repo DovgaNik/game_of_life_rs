c75bb6c67645a3ab8b37a983bf80d8e032f41db6
DovgaNik <nikita.dovgan.05@hotmail.com>
2024-07-26 13:55:04 +0300

    Implemented the looping logic

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,87 @@
 use rand::Rng;
+use std::thread::{self, sleep};
+use std::time::Duration;
 
-const MAP_SIZE: usize = 10;
-const P_TRUE: f64 = 1.0;
+const MAP_SIZE: usize = 20;
+const P_TRUE: f64 = 0.3;
 
 fn main() {
     let mut a: [[bool; MAP_SIZE]; MAP_SIZE] = [[false; MAP_SIZE]; MAP_SIZE];
+    let mut b: [[bool; MAP_SIZE]; MAP_SIZE] = [[false; MAP_SIZE]; MAP_SIZE];
 
     init_map(&mut a);
-    print_screen(a);
 
-    for x in 0..MAP_SIZE {
-        for y in 0..MAP_SIZE {
-            let mut n_alive: u8 = 0;
+    loop {
+        print_screen(a);
 
-            let x_i;
-            let y_i;
+        for x in 0..MAP_SIZE {
+            for y in 0..MAP_SIZE {
+                let mut n_alive: u8 = 0;
 
-            if x > 0 {
-                x_i = x - 1;
-            } else {
-                x_i = 0;
-            }
+                let x_i;
+                let y_i;
 
-            if y > 0 {
-                y_i = y - 1;
-            } else {
-                y_i = 0;
-            }
+                if x > 0 {
+                    x_i = x - 1;
+                } else {
+                    x_i = 0;
+                }
 
-            let x_n;
-            let y_n;
+                if y > 0 {
+                    y_i = y - 1;
+                } else {
+                    y_i = 0;
+                }
 
-            if x < MAP_SIZE - 1 {
-                x_n = x + 2;
-            } else {
-                x_n = MAP_SIZE;
-            }
+                let x_n;
+                let y_n;
 
-            if y < MAP_SIZE - 1 {
-                y_n = y + 2;
-            } else {
-                y_n = MAP_SIZE;
-            }
+                if x < MAP_SIZE - 1 {
+                    x_n = x + 2;
+                } else {
+                    x_n = MAP_SIZE;
+                }
 
-            for x_ in x_i..x_n {
-                for y_ in y_i..y_n {
-                    if a[x_][y_] == true && (x_ != x || y_ != y) {
-                        n_alive += 1;
+                if y < MAP_SIZE - 1 {
+                    y_n = y + 2;
+                } else {
+                    y_n = MAP_SIZE;
+                }
+
+                for x_ in x_i..x_n {
+                    for y_ in y_i..y_n {
+                        if a[x_][y_] == true && (x_ != x || y_ != y) {
+                            n_alive += 1;
+                        }
                     }
                 }
+
+                // Rule 1: If a live cell has fewer then 2 neighboring live cells it dies
+                if a[x][y] == true && n_alive < 2 {
+                    b[x][y] = false;
+                }
+
+                // Rule 2: If a live cell has 2 or 3 neighboring live cells it continues to live
+                if a[x][y] == true && (n_alive == 2 || n_alive == 3) {
+                    b[x][y] = true;
+                }
+
+                // Rule 3: If a live cell has more than 3 neighboring live cells it dies from overpopulation
+                if a[x][y] == true && n_alive > 3 {
+                    b[x][y] = false;
+                }
+
+                // Rule 4: If a dead cell has exactly 3 neighboring live cells it becomes live
+                if a[x][y] == false && n_alive == 3 {
+                    b[x][y] = true;
+                }
             }
-
-            println!("({x}, {y}) n_alive = {n_alive}")
-
-            // Rule 1: if a live cell has fewer then 2 neighboring live cells it dies
         }
-    }
 
-    // print_screen(a);
+        a = b;
+        b = [[false; MAP_SIZE]; MAP_SIZE];
+        thread::sleep(Duration::from_secs(1));
+    }
 }
 
 fn init_map(a: &mut [[bool; MAP_SIZE]; MAP_SIZE]) {
@@ -74,7 +98,7 @@ fn print_screen(a: [[bool; MAP_SIZE]; MAP_SIZE]) {
     for sub_arr in a {
         for element in sub_arr {
             if element == true {
-                print!("11");
+                print!("██");
             } else {
                 print!("  ");
             }
