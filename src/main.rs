6486429e9263cfa24480fa92318b700b7eda1b7a
DovgaNik <nikita.dovgan.05@hotmail.com>
2024-11-14 16:03:30 +0100

    Added a function to check amount of neighbors of a cell based on its coordinates and a linked list of all the true points

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
-use rand::Rng;
-use std::thread::{self, sleep};
-use std::time::Duration;
 use std::collections::{LinkedList};
 
 const MAP_SIZE: usize = 25;
@@ -8,7 +5,7 @@ const T_FRAME: u64 = 100;
 
 fn main() {
     let mut cells_alive_a: LinkedList<(i32, i32)> = LinkedList::new();
-    let mut cells_alive_b: LinkedList<(i32, i32)> = LinkedList::new();
+    let mut cells_alive_b: LinkedList<(i32, i32)>;
 
     cells_alive_a.push_back((0, 1));
     cells_alive_a.push_back((1, 1));
@@ -17,11 +14,24 @@ fn main() {
     cells_alive_a.push_back((2, 2));
 
     for &cell in &cells_alive_a {
-        let mut n_alive: u8 = 0;
-        let mut neighbors: LinkedList<(i32, i32)> = calculate_neighbors(cell);
-        
+        check_cell(cell, &cells_alive_a);
     }
 
+}
+
+fn check_cell(cell: (i32, i32), cells_alive: &LinkedList<(i32, i32)>) -> u8 {
+    let mut n_alive: u8 = 0;
+    let neighbors: LinkedList<(i32, i32)> = calculate_neighbors(cell);
+
+    for neighboring_cell in cells_alive {
+        for neighbor in &neighbors {
+            if (*neighboring_cell == *neighbor) {
+                n_alive += 1;
+            }
+        }
+    }
+    println!("({}, {}) n_alive={}", cell.0, cell.1, n_alive);
+    return(n_alive);
 }
 
 fn calculate_neighbors (cell: (i32, i32)) -> LinkedList<(i32, i32)> {
