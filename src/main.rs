a84b43a7bdbbaad00308b3368fd1705eaf9644d4
DovgaNik <nikita.dovgan.05@hotmail.com>
2024-11-14 18:58:03 +0100

    Full rework of code to work with vectors and HashSet

--- src/main.rs
+++ src/main.rs
@@ -1,110 +1,78 @@
-use std::collections::{HashSet, LinkedList};
-
-const MAP_SIZE: usize = 25;
-const T_FRAME: u64 = 100;
+use std::collections::{HashSet, VecDeque};
 
 fn main() {
-    let mut cells_alive_a: LinkedList<(i32, i32)> = init_map();
-    let mut cells_alive_b: LinkedList<(i32, i32)> = init_map();
+    let mut cells_alive_a: HashSet<(i128, i128)> = init_map();
+    let mut cells_alive_b: HashSet<(i128, i128)> = HashSet::new();
 
-    let mut counter = 0;
-    // Main loop
+    let mut counter: u128 = 0;
+    
     loop {
-        println!("n_alive = {}", cells_alive_a.len());
-        let mut cells_to_check: LinkedList<(i32, i32)> = LinkedList::new();
+        println!("n_iter = {}, n_alive = {}", counter, cells_alive_a.len());
+
+        let mut cells_to_check: HashSet<(i128, i128)> = HashSet::new();
         for &cell in &cells_alive_a {
-             cells_to_check.append(&mut calculate_neighbors(cell));
+            for neighbor in calculate_neighbors(cell) {
+                cells_to_check.insert(neighbor);
+            }
         }
-        remove_duplicates(&mut cells_to_check);
-        
+
         for &cell in &cells_to_check {
-            
-            let n_alive: u8 = check_cell(cell, &cells_alive_a);
-            
+            let n_alive = check_cell(cell, &cells_alive_a);
+
             // Rule 1: live cell with fewer than two live neighbors dies
             // Rule 2: live cell with 2 or 3 lives
             // Rule 3: live cell with more than 3 dies
             // Rule 4: dead cell with three becomes alive
-            
-            if check_element_in_list(cell, &cells_alive_a) {
+           
+            if cells_alive_a.contains(&cell) {
                 if n_alive == 2 || n_alive == 3 {
-                    cells_alive_b.push_back(cell);
+                    cells_alive_b.insert(cell);
                 }
             } else {
                 if n_alive == 3 {
-                    cells_alive_b.push_back(cell);
+                    cells_alive_b.insert(cell);
                 }
-            } 
-            
-        }
-        
-        cells_alive_a = cells_alive_b.clone();
-        cells_alive_b = LinkedList::new();
-        counter += 1;
-        if counter > 50 {
-            break
-        } 
-    } 
-
-}
-
-fn check_element_in_list (element: (i32, i32), list: &LinkedList<(i32, i32)>) -> bool {
-    for &item in list {
-        if (item == element) {
-            return true;
-        }
-    }
-    return false;
-}
-
-fn check_cell(cell: (i32, i32), cells_alive: &LinkedList<(i32, i32)>) -> u8 {
-    let mut n_alive: u8 = 0;
-    let neighbors: LinkedList<(i32, i32)> = calculate_neighbors(cell);
-
-    for neighboring_cell in cells_alive {
-        for neighbor in &neighbors {
-            if (*neighboring_cell == *neighbor) {
-                n_alive += 1;
             }
         }
+
+        cells_alive_a = cells_alive_b.clone();
+        cells_alive_b.clear();
+        counter += 1;
     }
-    return(n_alive);
 }
 
-fn calculate_neighbors (cell: (i32, i32)) -> LinkedList<(i32, i32)> {
-    let mut neighbors: LinkedList<(i32, i32)> = LinkedList::new();
-    neighbors.push_back((cell.0 - 1, cell.1 - 1));
-    neighbors.push_back((cell.0 - 1, cell.1));
-    neighbors.push_back((cell.0 - 1, cell.1 + 1));
-    neighbors.push_back((cell.0 + 1, cell.1 - 1));
-    neighbors.push_back((cell.0 + 1, cell.1));
-    neighbors.push_back((cell.0 + 1, cell.1 + 1));
-    neighbors.push_back((cell.0, cell.1 - 1));
-    neighbors.push_back((cell.0, cell.1 + 1));
-    return (neighbors);
-}
+fn check_cell(cell: (i128, i128), cells_alive: &HashSet<(i128, i128)>) -> u8 {
+    let neighbors = calculate_neighbors(cell);
+    let mut n_alive = 0;
 
-fn init_map() -> LinkedList<(i32, i32)> {
-    let mut cells_alive: LinkedList<(i32, i32)> = LinkedList::new();
-
-    cells_alive.push_back((0, 1));
-    cells_alive.push_back((1, 1));
-    cells_alive.push_back((1, 0));
-    cells_alive.push_back((1, 2));
-    cells_alive.push_back((2, 2));   
-    
-    return (cells_alive)
-}
-
-fn remove_duplicates(list: &mut LinkedList<(i32, i32)>) {
-    let mut seen = HashSet::new();
-    let mut unique_list = LinkedList::new();
-
-    for &item in list.iter() {
-        if seen.insert(item) {
-            unique_list.push_back(item);
+    for neighbor in neighbors {
+        if cells_alive.contains(&neighbor) {
+            n_alive += 1;
         }
     }
+    n_alive
+}
 
-    *list = unique_list;
+fn calculate_neighbors(cell: (i128, i128)) -> Vec<(i128, i128)> {
+    vec![
+        (cell.0 - 1, cell.1 - 1),
+        (cell.0 - 1, cell.1),
+        (cell.0 - 1, cell.1 + 1),
+        (cell.0 + 1, cell.1 - 1),
+        (cell.0 + 1, cell.1),
+        (cell.0 + 1, cell.1 + 1),
+        (cell.0, cell.1 - 1),
+        (cell.0, cell.1 + 1),
+    ]
+}
+
+fn init_map() -> HashSet<(i128, i128)> {
+    let initial_cells = vec![
+        (0, 1),
+        (1, 1),
+        (1, 0),
+        (1, 2),
+        (2, 2),
+    ];
+    initial_cells.into_iter().collect()
 }
